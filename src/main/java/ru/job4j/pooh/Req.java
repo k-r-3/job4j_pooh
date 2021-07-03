4edb82949ca29afd4f1ff3957240e266d72ca774
k-r-3 <rke19922802@gmail.com>
2021-07-03 13:42:54 +0500

    2. Тестовое задание - проект "Pooh JMS" [#268841](pre)

--- src/main/java/ru/job4j/pooh/Req.java
+++ src/main/java/ru/job4j/pooh/Req.java
@@ -19,34 +19,27 @@ public class Req {
     }
 
     public static Req of(String content) {
-        String[] command = content.split("/");
-//        Pattern msg = Pattern.compile("!?.*[a-zA-Z]+$");
-//        Pattern msg = Pattern.compile("!?(.*)\\s.*$");
-//        Matcher mod = Pattern.compile("!?(\\w\\s+.)[a-z]+").matcher(content);
-//        Matcher nam = Pattern.compile("!?\\W{2}[a-z]+", Pattern.MULTILINE).matcher(content);
         Matcher msg = Pattern.compile("!?(.*)\\s.*$").matcher(content);
-        Matcher met = Pattern.compile("[A-Z]+").matcher(content);
-        Matcher mod = Pattern.compile("!?\\w[a-z]+", Pattern.MULTILINE).matcher(content);
+        Matcher met = Pattern.compile("\\w+").matcher(content);
+        Matcher mod = Pattern.compile("[a-z]+", Pattern.MULTILINE).matcher(content);
+        Matcher nam = Pattern.compile("\\w+\\W+\\w+\\W(\\w+)", Pattern.MULTILINE).matcher(content);
         Matcher dig = Pattern.compile("\\d", Pattern.MULTILINE).matcher(content);
-        String m = "";
-        String mo = "";
-        String na = "";
+        String method = "";
+        String mode = "";
+        String name = "";
         int id = -1;
         if (met.find()) {
-            m = met.group();
+            method = met.group();
         }
         if (mod.find()) {
-            mo = mod.group();
+            mode = mod.group();
         }
-//        if (nam.find()) {
-//            na = nam.group();
-//        }
+        if (nam.find()) {
+            name = nam.group(1);
+        }
         if (dig.find()) {
             id = Integer.parseInt(dig.group().strip());
         }
-        String method = m;
-        String mode = mo;
-        String name = command[2];
         StringBuilder message = new StringBuilder(" ");
         if (msg.find()) {
             message.append(msg.group());
